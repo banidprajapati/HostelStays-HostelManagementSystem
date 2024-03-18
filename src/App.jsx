84f827baefbac4b808e56957d41b631853c47982
Banid Prajapati <banidprajapati98@gmail.com>
2024-03-18 07:27:31 +0545

    Update

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import {useState} from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-300 p-8">Hello World</h1>
+      <p>This is a paragraph</p>
+      <p>This is a paragraph</p>
+      <p>This is a paragraph</p>
+    </>
+  );
+}
+
+export default App;
