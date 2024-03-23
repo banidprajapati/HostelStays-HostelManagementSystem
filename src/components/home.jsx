4adb56b4249d23c12bb77c50382f5aa7ed67c3cc
KritikaTldr <tuladharkritika@gmail.com>
2024-03-23 21:32:54 +0545

    Create home.jsx

---
+++ src/components/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'; 
+export default const home = () => {
+  return (
+    <header>
+      <div className="company">Hostel Stays</div>
+        <button>Login</button>
+        <button>Signup</button>
+    </header>
+  );
+}
+
