0e93f0ccdf32111494af1e137657d46296451554
Banid Prajapati <banidprajapati98@gmail.com>
2024-03-25 17:28:06 +0545

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 // import {useState} from "react";
 import "./App.css";
 import {Home} from "./components/Home";
-
+import {Footer} from "./components/Footer";
 function App() {
   return (
     <>
       <Home />
+      <Footer />
     </>
   );
 }
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,8 +5,12 @@ export const Home = () => {
     <header className="flex justify-between items-center px-4 py-2">
       <div className="text-4xl text-red-600">Hostel Stays</div>
       <div>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2">Login</button>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Signup</button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2">
+          Login
+        </button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Signup
+        </button>
       </div>
     </header>
   );
