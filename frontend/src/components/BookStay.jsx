3d3ed77ea3bdb346fab2583a4703f9138f68dbb1
Aayush-33 <119646537+Aayush-33@users.noreply.github.com>
2024-04-06 13:56:52 +0545

    productp

--- frontend/src/components/BookStay.jsx
+++ frontend/src/components/BookStay.jsx
@@ -3,48 +3,64 @@ import React from "react";
 export const BookStay = () => {
   return (
     <div
-      className="flex flex-col items-start mt-8"
-      style={{ width: "80%", height: "400px" }}
+      className="flex flex-col items-start mt-2" // Adjusted margin of flex container
+      style={{ width: "90%", height: "300px" }} // Decreased height to 300px
     >
-      <h2 className="text-3xl font-bold">
+      <h2 className="text-3xl font-bold mb-2">
+        {/* Adjusted margin of heading */}
         <span className="text-black">Book</span> your stay
       </h2>
       <div
-        className="relative bg-gray-300 p-8 border rounded-lg w-full mx-auto"
+        className="relative bg-gray-300 p-8 border rounded-lg w-full mx-auto flex flex-col justify-between" // Adjusted width to full width, added flex properties
         style={{ height: "calc(100% - 40px)" }}
       >
-        <div className="absolute top-16 left-16 w-3/4 h-10 flex border rounded px-2">
+        <div className="flex justify-between mb-4">
           <input
             type="text"
-            className="w-1/3 h-10 bg-gray-200 px-2 focus:outline-none"
+            className="w-1/3 h-10 bg-gray-200 px-2 focus:outline-none rounded"
             placeholder="Date"
           />
-          <div className="w-px bg-gray-500"></div>
           <input
             type="text"
-            className="w-1/3 h-10 bg-gray-200 px-2 focus:outline-none"
+            className="w-1/3 h-10 bg-gray-200 px-2 focus:outline-none rounded"
             placeholder="Guests"
           />
-          <div className="w-px bg-gray-500 "></div>
           <input
             type="text"
-            className="w-1/3 h-10 bg-gray-200 px-2 focus:outline-none"
+            className="w-1/3 h-10 bg-gray-200 px-2 focus:outline-none rounded"
             placeholder="Rooms"
           />
         </div>
-        <hr className="absolute top-32 left-16 w-3/4 border border-white" />
-        <h3 className="absolute top-36 left-16 font-bold text-lg text-black">
-          Total Price (including all taxes)
-        </h3>
-        <h4 className="absolute top-44 left-16 text-black">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </h4>
-        <h3 className="absolute bottom-16 right-12 font-bold text-lg text-black">
-          NPR 2500
-        </h3>
-        <button className="absolute bottom-4 right-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-          Confirm Booking
-        </button>
+        <hr className="border border-white" />
+        <div className="flex flex-col">
+          {/* Adjusted margin-top */}
+          <h3
+            className="font-bold text-lg text-black"
+            style={{ textAlign: "left", marginBottom: "1px" }}
+          >
+            {/* Adjusted marginBottom */}
+            Total Price (including all taxes)
+          </h3>
+          <h4
+            className="text-black"
+            style={{ textAlign: "left", marginBottom: "35px" }}
+          >
+            {/* Adjusted marginBottom */}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </h4>
+          <h3
+            className="font-bold text-lg text-black"
+            style={{ paddingLeft: "260px" }} // Add padding to the left
+          >
+            NPR 2500
+          </h3>
+        </div>
+        <div className="flex justify-end">
+          {/* Adjusted to justify-end */}
+          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+            Confirm Booking
+          </button>
+        </div>
       </div>
     </div>
   );
--- frontend/src/components/Gallery.jsx
+++ frontend/src/components/Gallery.jsx
@@ -23,18 +23,17 @@ export const Gallery = () => {
     );
   };
 
-  // Filter the images array to include only the first 3 images
   const displayImages = images.slice(0, 3);
 
   return (
     <div className="text-left">
       <h2 className="text-3xl font-bold">
-        <span>Gal</span>lery
+        <span>Gallery</span>
       </h2>
       <br />
       <div
-        className="p-1 border rounded-lg w-3/5 mx-auto h-60 py-6 relative"
-        style={{ marginBottom: "-0.25rem" }}
+        className="p-1 border rounded-lg w-3/5 mx-auto h-80 py-6 relative"
+        style={{ marginBottom: "-0.25rem", width: "80%" }}
       >
         <img
           src={images[currentImageIndex]}
@@ -54,14 +53,13 @@ export const Gallery = () => {
           &gt;
         </button>
       </div>
-      {/* Render only the first 3 images */}
-      <div className="flex justify-center mt-2" style={{ margin: "-0.25rem" }}>
+      <div className="flex justify-center mt-4" style={{ margin: "-0.25rem" }}>
         {displayImages.map((image, index) => (
           <img
             key={index}
             src={image}
             alt={`Image ${index + 1}`}
-            className="p-1 border cursor-pointer object-cover w-24 h-24 rounded-lg m-1"
+            className="p-1 border cursor-pointer object-cover w-32 h-32 rounded-lg mx-4 my-2" // Adjusted margin to create space between images
             onClick={() => setCurrentImageIndex(index)}
           />
         ))}
