0604391b1e55ed6cbd20257a91afed2bf12448ff
KritikaTldr <tuladharkritika@gmail.com>
2024-04-14 22:34:53 +0545

    Navigate to Other Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import {Routes, Route} from "react-router-dom";
 import "./App.css";
-import { SignUp } from "./components/Signup";
-import { Login } from "./components/Login";
+import {SearchPage} from "./components/SearchPage";
+import {Login} from "./components/Login";
+import {LandingPage} from "./components/LandingPage";
+import {AdminLogin} from "./components/Admin/AdminLogin";
+import {SignUp} from "./components/Signup";
+import {AdminDashboard} from "./components/Admin/AdminDashboard"
+
 function App() {
   return (
     <>
-      <SignUp/>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/admin" element={<AdminLogin />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/dashboard" element={<AdminDashboard />} />
+        
+        {/* Add more routes as needed */}
+      </Routes>
+      {/* SignUp component is rendered outside of the Routes */}
+      <SignUp />
     </>
   );
 }
